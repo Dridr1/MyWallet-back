125074b1314edb5961eff6ea3393c70807201716
Adriano Fonseca <adrianof@pop-os.localdomain>
2022-02-03 23:43:28 -0300

    added changes to index file to implement the new architecture

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import express from "express";
 import cors from "cors";
-import { signIn, signUp } from "./controllers/authController.js";
+import authRouter from "./routes/authRouter.js";
+import movementsRouter from "./routes/movementsRouter.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-app.post('/sign-up', signUp);
-
-app.post('/sign-in', signIn);
+app.use(authRouter);
+app.use(movementsRouter)
 
 app.listen(5000, () => {
-    console.log("|--------------------------------------------------------------------|");
-    console.log("| Welcome to MyWallet :) API is running at http://localhost:5000/ |");
-    console.log("|--------------------------------------------------------------------|");
+    console.log("|---------------------------------------------------------------|");
+    console.log("| Welcome to MyWallet! API is running at http://localhost:5000/ |");
+    console.log("|---------------------------------------------------------------|");
 });
