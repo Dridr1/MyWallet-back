0f3109ad03e56d08c631c880e5437d8a9a38f6c0
Adriano Fonseca <a.driano@outlook.com>
2022-02-07 14:03:26 -0300

    updated schemas valid entries

--- src/schemas/movementSchema.js
+++ src/schemas/movementSchema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const movementSchema = joi.object({
     description: joi.string().required(),
-    value: joi.number().required(),
+    value: joi.number().min(0).required(),
     type: joi.string().required()
 });
 
--- src/schemas/newUserSchema.js
+++ src/schemas/newUserSchema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const newUserSchema = joi.object({
     email: joi.string().email().required(),
     name: joi.string().required().alphanum().max(30),
-    password: joi.string().required().min(6).max(30).alphanum(),
+    password: joi.string().required().min(6).max(30),
     repeat_password: joi.ref('password')
 });
 
