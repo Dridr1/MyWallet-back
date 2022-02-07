6f72f510f17941d96bdb81a0f216e8c99b08e88f
Adriano Fonseca <a.driano@outlook.com>
2022-02-07 14:02:46 -0300

    added delete and update movements routes

--- src/routes/movementsRouter.js
+++ src/routes/movementsRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getMovements, newMovement } from "../controllers/movementController.js";
+import { deleteMovement, getMovements, newMovement, updateMovement } from "../controllers/movementController.js";
 import { verifyTokenMiddleware } from "../middlewares/verifyTokenMiddleware.js";
-
+import { validateNewMovementMiddleware } from "../middlewares/validateNewMovementMiddleware.js";
 const movementsRouter = Router();
 
-movementsRouter.post('/movements', verifyTokenMiddleware, newMovement);
+movementsRouter.post('/movements', verifyTokenMiddleware, validateNewMovementMiddleware, newMovement);
 movementsRouter.get('/movements', verifyTokenMiddleware, getMovements);
-
+movementsRouter.delete("/movements", verifyTokenMiddleware, deleteMovement);
+movementsRouter.put("/movements", verifyTokenMiddleware, validateNewMovementMiddleware,updateMovement);
 export default movementsRouter;
