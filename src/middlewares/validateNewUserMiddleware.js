e1654a611a7e17d0348bbae7484e6c9fbc2089e8
leo <celsolnv27@gmail.com>
2022-02-11 11:45:37 -0300

    Preparando revisão de código

--- src/middlewares/validateNewUserMiddleware.js
+++
@@ -1,8 +0,0 @@
-import newUserSchema from "../schemas/newUserSchema.js";
-
-export const validateNewUserMiddleware = (req, res, next) => {
-    const user = req.body;
-    const validate = newUserSchema.validate(user);
-    if (validate.error) return res.status(422).send(validate.error);
-    next();
-}
